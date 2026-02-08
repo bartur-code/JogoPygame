16b8a65ba326282b28fe915eb1d1deb157bb9b17
bartur-code <b.artur@escolar.ifrn.edu.br>
2026-02-08 20:43:48 +0000

    nono

--- PyGame.py
+++ PyGame.py
@@ -47,3 +47,34 @@ def criar_blocos(fase):
             cor = random.choice(CORES_BLOCOS)
             blocos.append((bloco, cor))
     return blocos
+
+# Botões
+botao_reiniciar = pygame.Rect(300, 350, 200, 50)
+
+def jogo():
+    barra = pygame.Rect(350, 550, 100, 15)
+    bola = pygame.Rect(390, 535, 15, 15)
+
+    fase = 1
+    vidas = 3
+    pontos = 0
+    bola_lancada = False
+
+    vel_x = 0
+    vel_y = 0
+
+    blocos = criar_blocos(fase)
+    venceu = False
+    game_over = False
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if (game_over or venceu) and event.type == pygame.MOUSEBUTTONDOWN:
+                if botao_reiniciar.collidepoint(event.pos):
+                    return
+
+        teclas = pygame.key.get_pressed()
